1434c0dbcc3e69ae8a7ee72237a897c0339844c4
lhl <289041280@qq.com>
2022-03-06 13:54:52 +0800

    整理

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,18 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "src"),
+    },
+    hot: true,
+    open: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
